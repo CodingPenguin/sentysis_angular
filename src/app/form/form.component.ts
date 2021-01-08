cd4e2721fe5a472c7d2e98411b17aa3753537b5b
CodingPenguin <nguyenhuydanny@gmail.com>
2021-01-08 15:47:37 -0600

    Removed unnecessary comments

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -47,28 +47,4 @@ export class FormComponent implements OnInit {
     this.youtubeForm.reset();
   }
 
-
-/*  getVideoId(ytUrl) {
-    let regExp = /(youtu(?:\.be|be\.com)\/(?:.*v(?:\/|=)|(?:.*\/)?)([\w'-]+))/i;
-    if(!ytUrl.match(regExp)) {
-      alert("There was an error. Please make sure the URL is a YouTube URL.");
-      return "";
-    }
-    this.videoId = this.extractVideoId(ytUrl);
-    console.log("Verified and Extracted");
-    return this.videoId;
-  }
-
-  extractVideoId(ytUrl) {
-    let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/     /*;
-    let match = ytUrl.match(regExp);
-    if (match && match[7].length == 11) {
-      console.log(match[7]);
-      return match[7];
-    }
-    else {
-      alert("Could not extract video ID.");
-      return "";
-    }
-  }*/
 }
